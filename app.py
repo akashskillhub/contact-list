75e7d84446a66b65b1c6e15d7795dabbfa45a067
Akash Dhone <akashdhone01@gmail.com>
2026-07-22 18:20:31 +0530

    me route added

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def home():
     return {"message":"Flask Server Running Successfully"}
 
+@app.route("/me")
+def home():
+    return {"message":"john doe"}
+
 if __name__ == "__main__":
     app.run(debug=True)
